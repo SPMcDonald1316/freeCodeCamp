7431339c92920f1bdddf0187fa2e409e9bb50a56
sean <spmcdonald1316@gmail.com>
2024-03-12 16:26:04 -0500

    create easter egg

--- role_playing_game/script.js
+++ role_playing_game/script.js
@@ -97,6 +97,12 @@ const locations = [
     buttonText: ['REPLAY?', 'REPLAY?', 'REPLAY?'],
     buttonFunctions: [restart, restart, restart],
     text: 'You defeat the dragon! YOU WIN THE GAME! &#x1F389;'
+  },
+  {
+    name: 'easter egg',
+    buttonText: ['2', '8', 'Go to town square?'],
+    buttonFunctions: [pickTwo, pickEight, goTown],
+    text: 'You find a secret game. Pick a number above. Ten numbers will be randomly chosen between 0 and 10. If the number you choose matches one of the random numbers, you win!'
   }
 ];
 
@@ -235,4 +241,31 @@ const restart = () => {
   healthText.innerText = health;
   goldText.innerText = gold;
   goTown();
+}
+const easterEgg = () => {
+  update(locations[7]);
+}
+const pickTwo = () => pick(2);
+const pickEight = () => pick(8);
+const pick = guess => {
+  const numbers = [];
+  while (numbers.length < 10) {
+    numbers.push(Math.floor(Math.random() * 11));
+  }
+  text.innerText = `You picked ${guess}. Here are the random numbers: \n`;
+  for (let i = 0; i < 10; i++) {
+    text.innerText += `${numbers[i]}\n`;
+  }
+  if (numbers.includes(guess)) {
+    text.innerText += 'Right! You win 20 gold!';
+    gold += 20;
+    goldText.innerText = gold;
+  } else {
+    text.innerText += 'Wrong! You lose 10 health';
+    health -= 10;
+    healthText.innerText = health;
+    if (health <= 0) {
+      lose();
+    }
+  }
 }

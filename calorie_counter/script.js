1c842367d382311f21aeb50bed44de8f93be493a
sean <spmcdonald1316@gmail.com>
2024-03-13 16:58:31 -0500

    add function to clean input strings

--- calorie_counter/script.js
+++ calorie_counter/script.js
@@ -5,3 +5,19 @@ const addEntryButton = document.getElementById('add-entry');
 const clearButton = document.getElementById('clear');
 const output = document.getElementById('output');
 let isError = false;
+
+const cleanInputString = str => {
+  /* Loop to clean string input
+  Inefficient for memory & runtime
+
+  const strArray = str.split('');
+  const clearStrArray = [];
+  for (let i = 0; i < strArray.length; i++) {
+    if(!['+', '-', ' '].includes(strArray[i])) {
+      cleanStrArray.push(strArray[i]);
+    }
+  } */
+  // More efficient regex version
+  const regex = /[+-\s]/g;
+  return str.replace(regex, '');
+}

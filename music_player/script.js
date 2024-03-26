0847d217883462897f5cf63e32eab8f2abe09d3a
sean <spmcdonald1316@gmail.com>
2024-03-25 20:37:19 -0500

    create shuffle function, add event listener to shuffle button

--- music_player/script.js
+++ music_player/script.js
@@ -130,6 +130,15 @@ const playPreviousSong = () => {
   }
 };
 
+const shuffle = () => {
+  userData?.songs.sort(() => Math.random() - 0.5);
+  userData.currentSong = null;
+  userData.songCurrentTime = 0;
+  pauseSong();
+  setPlayerDisplay();
+  setPlayButtonAccessibleText();
+}
+
 const setPlayerDisplay = () => {
   const playingSong = document.getElementById('player-song-title');
   const songArtist = document.getElementById('player-song-artist');
@@ -220,6 +229,8 @@ pauseButton.addEventListener('click', pauseSong);
 nextButton.addEventListener('click', playNextSong);
 
 previousButton.addEventListener('click', playPreviousSong);
+
+shuffleButton.addEventListener('click', shuffle);
 
 const sortSongs = () => {
   userData?.songs.sort((a, b) => {

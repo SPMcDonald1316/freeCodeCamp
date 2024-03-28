58ae54c588cd9f0f4b733c024a30e74964c9aba8
sean <spmcdonald1316@gmail.com>
2024-03-28 17:15:37 -0500

    create function to display player information in cards

--- football_team_cards/script.js
+++ football_team_cards/script.js
@@ -179,3 +179,16 @@ teamName.textContent = team;
 typeOfSport.textContent = sport;
 worldCupYear.textContent = year;
 headCoach.textContent = coachName;
+
+const setPlayerCards = (arr = players) => {
+  playerCards.innerHTML += arr.map(({ name, position, number, isCaptain, nickname }) => 
+    `
+    <div class="player-card">
+      <h2>${name} ${isCaptain ? "(Captain)" : ""}</h2>
+      <p>Position: ${position}</p>
+      <p>Number: ${number}</p>
+      <p>Nickname: ${nickname ? nickname : "N/A"}</p>
+    </div>
+    `
+  ).join("");
+};

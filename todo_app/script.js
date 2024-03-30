42cdd6c2973508d6c87ff13a2b8e556c147022d6
sean <spmcdonald1316@gmail.com>
2024-03-30 09:32:23 -0500

    continue accessing html elements

--- todo_app/script.js
+++ todo_app/script.js
@@ -4,3 +4,9 @@ const openTaskFormBtn = document.getElementById('open-task-form-btn');
 const closeTaskFormBtn = document.getElementById('close-task-form-btn');
 const addOrUpdateTaskBtn = document.getElementById('add-or-update-task-form-btn');
 const cancelBtn = document.getElementById('cancel-btn');
+const discardBtn = document.getElementById('discard-btn');
+const tasksContainer = document.getElementById('tasks-container');
+const titleInput = document.getElementById('title-input');
+const dateInput = document.getElementById('date-input');
+const descriptionInput = document.getElementById('description-input');
+

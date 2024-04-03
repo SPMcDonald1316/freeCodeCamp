10d43774e8201b5229ef92efd107d007e3339605
sean <spmcdonald1316@gmail.com>
2024-04-02 20:39:07 -0500

    create delete task function

--- todo_app/script.js
+++ todo_app/script.js
@@ -45,7 +45,14 @@ const updateTaskContainer = () => {
       `)
     }
   );
-}
+};
+
+const deleteTask = buttonEl => {
+  const dataArrIndex = taskData.findIndex(item => item.id === buttonEl.parentElement.id);
+
+  buttonEl.parentElement.remove();
+  taskData.splice(dataArrIndex, 1);
+};
 
 const reset = () => {
   titleInput.value = '';

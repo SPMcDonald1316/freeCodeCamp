25759c30418fea2efd1e706fa75c3f4b8f4b4253
sean <spmcdonald1316@gmail.com>
2024-04-03 17:04:04 -0500

    create function to check for appropriate input

--- decimal_binary_converter/script.js
+++ decimal_binary_converter/script.js
@@ -2,6 +2,13 @@ const numberInput = document.getElementById('number-input');
 const convertBtn = document.getElementById('convert-btn');
 const result = document.getElementById('result');
 
+const checkUserInput = () => {
+  if (!numberInput.value || isNaN(parseInt(numberInput.value))) {
+    alert('Please provide a decimal number');
+    return;
+  };
+}
+
 convertBtn.addEventListener('click', checkUserInput);
 
 numberInput.addEventListener('keydown', e => {

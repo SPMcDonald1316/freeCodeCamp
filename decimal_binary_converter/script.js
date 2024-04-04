94b2ba8921ab723c98fcdaa56bcbdd47b77535ed
sean <spmcdonald1316@gmail.com>
2024-04-03 20:37:26 -0500

    refactor conversion func to be more efficient

--- decimal_binary_converter/script.js
+++ decimal_binary_converter/script.js
@@ -3,27 +3,17 @@ const convertBtn = document.getElementById('convert-btn');
 const result = document.getElementById('result');
 
 const decimalToBinary = input => {
-  const inputs = [];
-  const quotients = [];
-  const remainders = [];
+  let binary = '';
 
-  if (input === 0) {
-    result.innerText = '0';
-    return;
-  }
+  if (input === 0) binary = '0';
 
   while (input > 0) {
-    const quotient = Math.floor(input / 2);
-    const remainder = input % 2;
-
-    inputs.push(input);
-    quotients.push(quotient);
-    remainders.push(remainder);
-    input = quotient;
+    binary = (input % 2) + binary;
+    input = Math.floor(input / 2);
   }
 
-  result.innerText = remainders.reverse().join('');
-}
+  result.innerText = binary;
+};
 
 const checkUserInput = () => {
   if (!numberInput.value || isNaN(parseInt(numberInput.value))) {

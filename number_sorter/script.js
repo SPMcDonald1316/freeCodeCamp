4243370d2334ce255307257ba71766b9e18b28ec
sean <spmcdonald1316@gmail.com>
2024-04-09 16:34:05 -0500

    access elements from dropdown menus, convert to numbers and store in array

--- number_sorter/script.js
+++ number_sorter/script.js
@@ -2,6 +2,7 @@ const sortButton = document.getElementById('sort');
 
 const sortInputArray = e => {
   e.preventDefault();
+  const inputValues = [...document.getElementsByClassName('values-dropdown')].map(dropdown => Number(dropdown.value));
 }
 
 sortButton.addEventListener('click', sortInputArray);

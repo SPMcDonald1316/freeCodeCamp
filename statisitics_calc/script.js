f55e9925f892a0d3d25fcb6d67bb11b6b4e46bf4
sean <spmcdonald1316@gmail.com>
2024-04-18 16:26:34 -0500

    write func to calc median, call func in submit and display

--- statisitics_calc/script.js
+++ statisitics_calc/script.js
@@ -1,4 +1,12 @@
 const getMean = array => array.reduce((acc, el) => acc + el, 0) / array.length;
+const getMedian = array => {
+  const midIndex = array.length / 2; // calc once
+  const sorted = array.sort((a, b) => a - b);
+
+  const median = array.length % 2 === 0 ? 
+    getMean([sorted[midIndex], sorted[midIndex - 1]]) : sorted[midIndex];
+  return median;
+}
 
 const calculate = () => {
   const value = document.querySelector("#numbers").value;
@@ -6,5 +14,8 @@ const calculate = () => {
   const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
 
   const mean = getMean(numbers)
+  const median = getMedian(numbers)
+
   document.querySelector('#mean').textContent = mean;
+  document.querySelector('#median').textContent = median;
 }

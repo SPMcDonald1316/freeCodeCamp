f42540c7501d9b0172c17a529b1e2e53b5028549
sean <spmcdonald1316@gmail.com>
2024-04-18 16:57:37 -0500

    find range func, call in submit func and display

--- statisitics_calc/script.js
+++ statisitics_calc/script.js
@@ -20,6 +20,8 @@ const getMode = array => {
   return mode.join(', ');
 };
 
+const getRange = array => Math.max(...array) - Math.min(...array);
+
 const calculate = () => {
   const value = document.querySelector("#numbers").value;
   const array = value.split(/,\s*/g);
@@ -28,8 +30,10 @@ const calculate = () => {
   const mean = getMean(numbers);
   const median = getMedian(numbers);
   const mode = getMode(numbers);
+  const range = getRange(numbers);
 
   document.querySelector('#mean').textContent = mean;
   document.querySelector('#median').textContent = median;
   document.querySelector('#mode').textContent = mode;
+  document.querySelector('#range').textContent = range;
 }

e7e1cb124669ff5db5e28dbb9c3747b0b108607e
sean <spmcdonald1316@gmail.com>
2024-04-22 16:41:36 -0500

    func to calc std deviation, call and display

--- statisitics_calc/script.js
+++ statisitics_calc/script.js
@@ -32,6 +32,11 @@ const getVariance = array => {
   return variance;
 };
 
+const getStdDev = array => {
+  const variance = getVariance(array)
+  return Math.sqrt(variance);
+}
+
 const calculate = () => {
   const value = document.querySelector("#numbers").value;
   const array = value.split(/,\s*/g);
@@ -42,10 +47,12 @@ const calculate = () => {
   const mode = getMode(numbers);
   const range = getRange(numbers);
   const variance = getVariance(numbers);
+  const stdDev = getStdDev(numbers);
 
   document.querySelector('#mean').textContent = mean;
   document.querySelector('#median').textContent = median;
   document.querySelector('#mode').textContent = mode;
   document.querySelector('#range').textContent = range;
   document.querySelector('#variance').textContent = variance;
+  document.querySelector('#standardDeviation').textContent = stdDev;
 }

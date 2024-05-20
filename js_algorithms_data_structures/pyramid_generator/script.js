be6418ee694028c2527f2245e1cc2c50aa435c79
sean <spmcdonald1316@gmail.com>
2024-05-20 16:27:34 -0500

    create result var, concatenate each row of array to result string and display

--- js_algorithms_data_structures/pyramid_generator/script.js
+++ js_algorithms_data_structures/pyramid_generator/script.js
@@ -2,6 +2,14 @@ const character = "#";
 const count = 8;
 const rows = [];
 
-for (let i = 1; i < count; i++) {
+for (let i = 1; i <= count; i++) {
   rows.push(character.repeat(i))
 }
+
+let result = ""
+
+for (const row of rows) {
+  result += "\n" + row
+}
+
+console.log(result)

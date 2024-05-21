5a432ee94e1d353c7cd3bbf886bdb8f0bcefd0d2
sean <spmcdonald1316@gmail.com>
2024-05-21 15:53:36 -0500

    add vars to keep track of scores, create function to determine if player won round

--- js_algorithms_data_structures/rock_paper_scissors/script.js
+++ js_algorithms_data_structures/rock_paper_scissors/script.js
@@ -1,5 +1,16 @@
 function getRandomComputerResult() {
   const options = ["Rock", "Paper", "Scissors"];
-  const index = Math.floor(Math.random() * 3)
+  const index = Math.floor(Math.random() * options.length);
   return options[index];
+}
+
+let playerScore = 0;
+let computerScore = 0;
+
+function hasPlayerWonTheRound(player, computer) {
+  return (
+    (player === "Rock" && computer === "Scissors") ||
+    (player === "Scissors" && computer === "Paper") ||
+    (player === "Paper" && computer === "Rock")
+  )
 }

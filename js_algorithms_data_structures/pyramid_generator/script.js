5217d1aa9887a6c65a9cb1fa994a9a9ba3788c24
sean <spmcdonald1316@gmail.com>
2024-05-20 20:12:34 -0500

    make file read more clearly by moving function to top of file

--- js_algorithms_data_structures/pyramid_generator/script.js
+++ js_algorithms_data_structures/pyramid_generator/script.js
@@ -2,6 +2,13 @@ const character = "#";
 const count = 8;
 const rows = [];
 
+function padRow(rowNumber, rowCount) {
+  const spaces = " ".repeat(rowCount - rowNumber);
+  const charRepeat = 2 * rowNumber - 1;
+
+  return spaces + character.repeat(charRepeat) + spaces;
+}
+
 /* Add characters to rows with for loop
 for (let i = 1; i <= count; i++) {
   rows.push(padRow(i, count))
@@ -24,13 +31,6 @@ let result = ""
 
 for (const row of rows) {
   result += "\n" + row
-}
-
-function padRow(rowNumber, rowCount) {
-  const spaces = " ".repeat(rowCount - rowNumber);
-  const charRepeat = 2 * rowNumber - 1;
-
-  return spaces + character.repeat(charRepeat) + spaces;
 }
 
 console.log(result)

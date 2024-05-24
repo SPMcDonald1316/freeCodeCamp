ae3de81fdce1d5b929e2a74de599a1614fb64cc8
sean <spmcdonald1316@gmail.com>
2024-05-24 16:28:50 -0500

    use forEach loop on products array to create HTML snippets to display products to page

--- js_algorithms_data_structures/shopping_cart/script.js
+++ js_algorithms_data_structures/shopping_cart/script.js
@@ -84,3 +84,18 @@ const products = [
     category: "Cupcake",
   },
 ];
+
+products.forEach(
+  ({ name, id, price, category }) => {
+    dessertCards.innerHTML += `
+      <div class="dessert-cards">
+        <h2>${name}</h2>
+        <p class="dessert-price">$${price}</p>
+        <p class="product-category">Category: ${category}</p>
+        <button id="${id}" class="btn add-to-cart-btn">
+          Add to cart
+        </button>
+      </div>
+    `;
+  }
+);

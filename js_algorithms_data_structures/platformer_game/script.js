b7d19a930332bbb211f8a587817fd7f72cb19933
sean <spmcdonald1316@gmail.com>
2024-05-29 15:56:03 -0500

    setup canvas api

--- js_algorithms_data_structures/platformer_game/script.js
+++ js_algorithms_data_structures/platformer_game/script.js
@@ -5,4 +5,7 @@ const startScreen = document.querySelector(".start-screen");
 const checkpointScreen = document.querySelector(".checkpoint-screen");
 const checkpointMessage = document.querySelector(".checkpoint-screen > p");
 
-// 
+// Setup Canvas API
+const ctx = canvas.getContex("2d");
+canvas.width = innerWidth;
+canvas.height = innerHeight;

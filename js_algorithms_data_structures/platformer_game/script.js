55091aad2ed1beaf375c25e25bc098a08fcc9dc4
sean <spmcdonald1316@gmail.com>
2024-05-31 20:31:06 -0500

    check for final checkpoint, display message and reset player if true

--- js_algorithms_data_structures/platformer_game/script.js
+++ js_algorithms_data_structures/platformer_game/script.js
@@ -195,6 +195,12 @@ const animate = () => {
     if (checkpointDetectionRules.every(rule => rule)) {
       checkpoint.claim();
     }
+
+    if (index === checkpoints.length - 1) {
+      isCheckpointCollisionDetectionActive = false;
+      showCheckpointScreen("You reached the final checkpoint!");
+      movePlayer("ArrowRight", 0, false)
+    }
   })
 }
 

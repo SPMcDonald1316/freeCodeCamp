d8daee672b7ee671ac368268bebcaced69c1d899
sean <spmcdonald1316@gmail.com>
2024-05-31 20:06:28 -0500

    adjust checkpoint positions with player movement

--- js_algorithms_data_structures/platformer_game/script.js
+++ js_algorithms_data_structures/platformer_game/script.js
@@ -149,8 +149,10 @@ const animate = () => {
     player.velocity.x = 0;
     if (keys.rightKey.pressed && isCheckpointCollisionDetectionActive) {
       platforms.forEach(platform => platform.position.x -= 5);
+      checkpoints.forEach(checkpoint => checkpoint.position.x -= 5);
     } else if (keys.leftKey.pressed && isCheckpointCollisionDetectionActive) {
       platforms.forEach(platform => platform.position.x += 5);
+      checkpoints.forEach(checkpoint => checkpoint.position.x += 5);
     }
   }
 

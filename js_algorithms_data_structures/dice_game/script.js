45d9b7b2b9347f1f3cef7abfffe985944d66f254
sean <spmcdonald1316@gmail.com>
2024-06-04 16:58:29 -0500

    build function to reset game, call reset once 6 rounds have been completed

--- js_algorithms_data_structures/dice_game/script.js
+++ js_algorithms_data_structures/dice_game/script.js
@@ -95,7 +95,26 @@ const updateScore = (selectedValue, achieved) => {
   totalScoreText.textContent = totalScore;
 
   scoreHistory.innerHTML = `<li>${achieved} : ${selectedValue}</li>`;
-}
+};
+
+const resetGame = () => {
+  diceValuesArr = [0, 0, 0, 0, 0];
+  score = 0;
+  totalScore = 0;
+  rolls = 0;
+  round = 1;
+
+  listOfAllDice.forEach((dice, index) => {
+    dice.textContent = diceValuesArr[index];
+  });
+
+  totalScoreText.textContent = totalScore;
+  scoreHistory.innerHTML = "";
+  currentRoundRollsText.textContent = rolls;
+  currentRoundText.textContent = round;
+
+  resetRadioOption();
+};
 
 // Event Listeners
 rulesBtn.addEventListener("click", () => {
@@ -143,6 +162,7 @@ keepScoreBtn.addEventListener("click", () => {
     if (round > 6) {
       setTimeout(() => {
         alert(`Game Over! Your total score is ${totalScore}`);
+        resetGame();
       }, 500);
     } 
   } else {

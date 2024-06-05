29e4a1680c9fadc91688ac4090a087ddf4353d6b
sean <spmcdonald1316@gmail.com>
2024-06-05 16:16:29 -0500

    create vars

--- js_algorithms_data_structures/fCC_authors/script.js
+++ js_algorithms_data_structures/fCC_authors/script.js
@@ -1,6 +1,9 @@
 const authorContainer = document.getElementById("author-container");
 const loadMoreBtn = document.getElementById("load-more-btn");
 
+let start = 0;
+let end = 8;
+let authorData = [];
 
 fetch("https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json")
   .then(res => res.json())
@@ -8,3 +11,4 @@ fetch("https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json")
     console.log(data)
   })
   .catch(err => console.error(`There was an error: ${err}`));
+

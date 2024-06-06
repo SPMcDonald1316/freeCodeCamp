be4386aec708c77853586935c451c9adde532a5f
sean <spmcdonald1316@gmail.com>
2024-06-06 16:13:07 -0500

    build function to convert time since last activity

--- js_algorithms_data_structures/fCC_forum_leaderboard/script.js
+++ js_algorithms_data_structures/fCC_forum_leaderboard/script.js
@@ -1,10 +1,13 @@
+// URLs for Fetching Data
 const forumLatest = "https://cdn.freecodecamp.org/curriculum/forum-latest/latest.json";
 const forumTopicUrl = "https://forum.freecodecamp.org/t/";
 const forumCategoryUrl = "https://forum.freecodecamp.org/c/";
 const avatarUrl = "https://sea1.discourse-cdn.com/freecodecamp";
 
+// Accessed HTML Elements
 const postsContainer = document.getElementById("posts-container");
 
+// Data Fetch
 const fetchData = async () => {
   try {
     const res = await fetch(forumLatest);
@@ -17,6 +20,29 @@ const fetchData = async () => {
 
 fetchData();
 
+// Time Conversion for Last Activiy
+const lastActivity = time => {
+  const currentTime = new Date();
+  const lastPost = new Date(time);
+  const timePassed = currentTime - lastPost;
+
+  const msPerMin = 1000 * 60;
+
+  const minutes = Math.floor(timePassed / msPerMin);
+  const hours = Math.floor(minutes / 60);
+  const days = Math.floor(hours / 24);
+
+  if (minutes < 60) {
+    return `${minutes}m ago`;
+  }
+
+  if (hours < 24) {
+    return `${hours}h ago`;
+  }
+
+  return `${days}d ago`;
+};
+
 const showLatestPosts = data => {
   const { topic_list, users } = data;
   const { topics } = topic_list;
@@ -27,11 +53,11 @@ const showLatestPosts = data => {
       title,
       views,
       posts_count,
-      slud,
+      slug,
       posters_id,
       bumped_at
     } = item;
-
+co
     return `
       <tr>
         <td><p class="post-title">${title}</p></td>

f7e4a22f0485cd3b64ba63387724a71a09021b1a
sean <spmcdonald1316@gmail.com>
2024-06-05 19:59:41 -0500

    small fixes, complete project

--- js_algorithms_data_structures/fCC_authors/script.js
+++ js_algorithms_data_structures/fCC_authors/script.js
@@ -11,7 +11,9 @@ fetch("https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json")
     authorData = data;
     displayAuthors(authorData.slice(start, end));
   })
-  .catch(err => console.error(`There was an error: ${err}`));
+  .catch(err => {
+    authorContainer.innerHTML = `<p class="error-msg">There was an error loading the authors </p>`;
+  };
 
 const displayAuthors = authors => {
   authors.forEach(({author, image, url, bio}, index) => {
@@ -19,7 +21,7 @@ const displayAuthors = authors => {
       <div id="${index}" class="user-card">
         <h2 class="author-name">${author}</h2>
         <img class="user-img" src="${image}" alt="${author} avatar" />
-        <p class="bio">${bio}</p>
+        <p class="bio">${ bio.length > 50 ? bio.slice(0, 50) + '...' : bio}</p>
         <a class="author-link" href="${url}" target="_blank">${author}'s author page</a>
       </div>
     `;
@@ -30,6 +32,12 @@ const fetchMoreAuthors = () => {
   start += 8;
   end += 8;
   displayAuthors(authorData.slice(start, end));
+
+  if (authorData.length <= end) {
+    loadMoreBtn.disabled = true;
+    loadMoreBtn.textContent = "All authors displayed";
+    loadMoreBtn.style.cursor = "not-allowed";
+  }
 };
 
 loadMoreBtn.addEventListener("click", fetchMoreAuthors);

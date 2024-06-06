b129039178a367b49141b922a8d1fd8a521fbae0
sean <spmcdonald1316@gmail.com>
2024-06-05 20:30:12 -0500

    write asynchronus func to fetch data from api

--- js_algorithms_data_structures/fCC_forum_leaderboard/script.js
+++ js_algorithms_data_structures/fCC_forum_leaderboard/script.js
@@ -5,3 +5,11 @@ const avatarUrl = "https://sea1.discourse-cdn.com/freecodecamp";
 
 const postsContainer = document.getElementById("posts-container");
 
+const fetchData = async () => {
+  try {
+    const res = await fetch(forumLatest);
+    const data = await res.json();
+  } catch (err) {
+    console.log(err);
+  }
+}

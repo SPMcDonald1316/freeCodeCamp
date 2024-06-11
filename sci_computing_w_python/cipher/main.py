b95b44f1de6197a3d28fd2ec2794c341f75f6e30
sean <spmcdonald1316@gmail.com>
2024-06-11 11:42:43 -0400

    add vigenere cipher

--- sci_computing_w_python/cipher/main.py
+++ sci_computing_w_python/cipher/main.py
@@ -1,23 +1,55 @@
+# Global Variables
 text = 'Hello World'
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+print('Message to be encoded:', text)
+print()
+
+# Shift for caesar cipher
 shift = 3
 
 # Caesar Cipher
 def caesar(message, offset):
-  alphabet = 'abcdefghijklmnopqrstuvwxyz'
-  encrypted_text = ''
+  caesar_text = ''
 
   for char in message.lower():
     if char == ' ':
-      encrypted_text += char
+      caesar_text += char
     else:
       index = alphabet.find(char)
       new_index = (index + offset) % len(alphabet)
-      encrypted_text += alphabet[new_index]
+      caesar_text += alphabet[new_index]
 
-  print('plain text:', message)
-  print('encrypted text:', encrypted_text)
+  return caesar_text
 
-caesar(text, shift)
+print('Caesar encoding:', caesar(text, shift))
 print()
 
+# Key for vigenère cipher
+custom_key = 'python'
+
 # Vigenère Cipher
+def vigenere(message, key):
+  key_index = 0
+  vigenere_text = ''
+
+  for char in message.lower():
+    # Append space to the message
+    if char == ' ':
+      vigenere_text += char
+    else:
+      # Find the right key character to encode
+      key_char = key[key_index % len(key)]
+      key_index += 1
+
+      # Define the offset and the encrypted letter
+      offset = alphabet.index(key_char)
+      index = alphabet.find(char)
+      new_index = (index + offset) % len(alphabet)
+      vigenere_text += alphabet[new_index]
+  
+  return vigenere_text
+
+encryption = vigenere(text, custom_key)
+print('Vigenère encoding:', encryption)
+print()

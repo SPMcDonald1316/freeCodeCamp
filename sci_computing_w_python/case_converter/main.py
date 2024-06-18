430e33f40a793f81f4c41b511638d70982f1ddab
sean <spmcdonald1316@gmail.com>
2024-06-18 09:34:35 -0500

    write list comprehension version of snake case conversion function, project complete

--- sci_computing_w_python/case_converter/main.py
+++ sci_computing_w_python/case_converter/main.py
@@ -1,16 +1,26 @@
 # Loop Version
+# def convert_to_snake_case(str):
+#   converted_chars = []
+#   for char in str:
+#     if char.isupper():
+#       converted_char = f'_{char.lower()}'
+#       converted_chars.append(converted_char)
+#     else:
+#       converted_chars.append(char)
+
+#   # Join converted chars into string     
+#   snake_case_str = ''.join(converted_chars)
+#   # Strip of possible leading underscore and return.
+#   return snake_case_str.strip('_')
+
+# List Comprehension Version
 def convert_to_snake_case(str):
-  converted_chars = []
-  for char in str:
-    if char.isupper():
-      converted_char = f'_{char.lower()}'
-      converted_chars.append(converted_char)
-    else:
-      converted_chars.append(char)
+  snake_case_chars = ['_' + char.lower() if char.isupper() else char for char in str]
 
-  # Join converted chars into string     
-  snake_case_str = ''.join(converted_chars)
-  # Strip of possible leading underscore and return.
-  return snake_case_str.strip('_')
+  return ''.join(snake_case_chars).strip('_')
 
+def main():
+  print(convert_to_snake_case('IAmAPascalCasedString'))
+  print(convert_to_snake_case('iAmACamelCasedString'))
 
+main()

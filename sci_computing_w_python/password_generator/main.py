f7b4f5ae49ef1a274bf447b0ae91410844254cf0
sean <spmcdonald1316@gmail.com>
2024-06-24 17:11:32 -0500

    use loop to generate password based on user input for length

--- sci_computing_w_python/password_generator/main.py
+++ sci_computing_w_python/password_generator/main.py
@@ -9,3 +9,11 @@ def generate_password(length):
 
   # Combine all characters
   all_chars = letters + digits + symbols
+
+  #Generate password
+  password = ''
+  for _ in range(length):
+    password += secrets.choice(all_chars)
+
+  return password
+

330b220e5f771a085314536bdf5b5e2d48ab7f26
sean <spmcdonald1316@gmail.com>
2024-06-24 22:08:12 -0500

    refactor to use dictionary comprehensions instead of for loops

--- sci_computing_w_python/shortest_path/main.py
+++ sci_computing_w_python/shortest_path/main.py
@@ -2,16 +2,17 @@ my_graph = {
   'A': [('B', 3), ('D', 1)],
   'B': [('A', 3), ('C', 4)],
   'C': [('B', 4), ('D', 7)],
-  'D': [('A': 1), ('C', 7)]
+  'D': [('A', 1), ('C', 7)]
 }
 
 def shortest_path(graph, start):
-  unvisited = []
-  distances = {}
-  for node in graph:
-    unvisited.append(node)
-    if node == start:
-      distances[node] = 0
-    else:
-      distances[node] = float('inf')
-  print(f'Unvisited: {unvisited}\nDistances: {distances}')
+  unvisited = list(graph)
+  distances = {
+    node: 0 if node == start else float('inf')
+    for node in graph
+  }
+  paths = {node: [] for node in graph}
+  paths[start].append(start)
+  print(f'Unvisited: {unvisited}\nDistances: {distances}\nPaths: {paths}')
+
+shortest_path(my_graph, 'A')

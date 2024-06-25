f1199276f85fb75f9b3911ce9765ce6755d732d7
sean <spmcdonald1316@gmail.com>
2024-06-24 19:15:41 -0500

    add default args for password function, project complete

--- sci_computing_w_python/password_generator/main.py
+++ sci_computing_w_python/password_generator/main.py
@@ -2,7 +2,7 @@ import re
 import secrets
 import string
 
-def generate_password(length, nums, special_chars, uppercase, lowercase):
+def generate_password(length=16, nums=1, special_chars=1, uppercase=1, lowercase=1):
   # Define the possible chars for password
   letters = string.ascii_letters
   digits = string.digits
@@ -40,3 +40,7 @@ def generate_password(length, nums, special_chars, uppercase, lowercase):
 
   return password
 
+
+if __name__ == '__main__':
+  new_password = generate_password()
+  print('Generated password:', new_password)

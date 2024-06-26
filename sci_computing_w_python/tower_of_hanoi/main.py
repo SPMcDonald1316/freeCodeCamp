7ea147a4548080b0a25d7b37757841901ea3d9c4
sean <spmcdonald1316@gmail.com>
2024-06-26 11:07:38 -0500

    comment out iterative solution, begin recursive solution

--- sci_computing_w_python/tower_of_hanoi/main.py
+++ sci_computing_w_python/tower_of_hanoi/main.py
@@ -6,44 +6,50 @@ rods = {
   'C': []
 }
 
-def make_allowed_move(rod1, rod2):
-  forward = False
-  if not rods[rod2]:
-    forward = True
-  elif rods[rod1] and rods[rod1][-1] < rods[rod2][-1]:
-    forward = True
-  if forward:
-    print(f'Moving disk {rods[rod1][-1]} from {rod1} to {rod2}')
-    rods[rod2].append(rods[rod1].pop())
-  else:
-    print(f'Moving disk {rods[rod2][-1]} from {rod2} to {rod1}')
-    rods[rod1].append(rods[rod2].pop())
-  # display progress
-  print(rods)
+# ITERATIVE SOLUTION
+# def make_allowed_move(rod1, rod2):
+#   forward = False
+#   if not rods[rod2]:
+#     forward = True
+#   elif rods[rod1] and rods[rod1][-1] < rods[rod2][-1]:
+#     forward = True
+#   if forward:
+#     print(f'Moving disk {rods[rod1][-1]} from {rod1} to {rod2}')
+#     rods[rod2].append(rods[rod1].pop())
+#   else:
+#     print(f'Moving disk {rods[rod2][-1]} from {rod2} to {rod1}')
+#     rods[rod1].append(rods[rod2].pop())
+#   # display progress
+#   print(rods)
 
 
+# def move(n, source, auxiliary, target):
+#   # display starting configuration
+#   print(rods)
+#   for i in range(number_of_moves):
+#     remainder = (i + 1) % 3
+#     if remainder == 1:
+#       if n % 2 != 0:
+#         print(f'Move {i + 1} allowed between {source} and {target}')
+#         make_allowed_move(source, target)
+#       else:
+#         print(f'Move {i + 1} allowed between {source} and {auxiliary}')
+#         make_allowed_move(source, auxiliary)
+#     elif remainder == 2:
+#       if n % 2 != 0:
+#         print(f'Move {i + 1} allowed between {source} and {auxiliary}')
+#         make_allowed_move(source, auxiliary)
+#       else:
+#         print(f'Move {i + 1} allowed between {source} and {target}')
+#         make_allowed_move(source, target)
+#     elif remainder == 0:
+#       print(f'Move {i + 1} allowed between {auxiliary} and {target}')
+#       make_allowed_move(auxiliary, target)
+
+# RECURSIVE SOLUTION
 def move(n, source, auxiliary, target):
   # display starting configuration
-  print(rods)
-  for i in range(number_of_moves):
-    remainder = (i + 1) % 3
-    if remainder == 1:
-      if n % 2 != 0:
-        print(f'Move {i + 1} allowed between {source} and {target}')
-        make_allowed_move(source, target)
-      else:
-        print(f'Move {i + 1} allowed between {source} and {auxiliary}')
-        make_allowed_move(source, auxiliary)
-    elif remainder == 2:
-      if n % 2 != 0:
-        print(f'Move {i + 1} allowed between {source} and {auxiliary}')
-        make_allowed_move(source, auxiliary)
-      else:
-        print(f'Move {i + 1} allowed between {source} and {target}')
-        make_allowed_move(source, target)
-    elif remainder == 0:
-      print(f'Move {i + 1} allowed between {auxiliary} and {target}')
-      make_allowed_move(auxiliary, target)
-
+  print(rods, '\n')
+  
 # initiate call from source A to target C with aux B
 move(NUMBER_OF_DISKS, 'A', 'B', 'C')

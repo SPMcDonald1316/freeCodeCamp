b1ab30a8afc0f14910875cb0012f00d7b337add5
sean <spmcdonald1316@gmail.com>
2024-06-26 10:39:43 -0500

    refactor, move code for making moves to its own function, make function calls where appropriate

--- sci_computing_w_python/tower_of_hanoi/main.py
+++ sci_computing_w_python/tower_of_hanoi/main.py
@@ -6,6 +6,22 @@ rods = {
   'C': []
 }
 
+def make_allowed_move(rod1, rod2):
+  forward = False
+  if not rods[rod2]:
+    forward = True
+  elif rods[rod1] and rods[rod1][-1] < rods[rod2][-1]:
+    forward = True
+  if forward:
+    print(f'Moving disk {rods[rod1][-1]} from {rod1} to {rod2}')
+    rods[rod2].append(rods[rod1].pop())
+  else:
+    print(f'Moving disk {rods[rod2][-1]} from {rod2} to {rod1}')
+    rods[rod1].append(rods[rod2].pop())
+  # display progress
+  print(rods)
+
+
 def move(n, source, auxiliary, target):
   # display starting configuration
   print(rods)
@@ -13,23 +29,13 @@ def move(n, source, auxiliary, target):
     remainder = (i + 1) % 3
     if remainder == 1:
       print(f'Move {i + 1} allowed between {source} and {target}')
-      forward = False
-      if not rods[target]:
-        forward = True
-      elif rods[source] and rods[source][-1] < rods[target][-1]:
-        forward = True
-      if forward:
-        print(f'Moving disk {rods[source][-1]} from {source} to {target}')
-        rods[target].append(rods[source].pop())
-      else:
-        print(f'Moving disk {rods[target][-1]} from {target} to {source}')
-        rods[source].append(rods[target].pop())
-      # display progress
-      print(rods)
+      make_allowed_move(source, target)
     elif remainder == 2:
       print(f'Move {i + 1} allowed between {source} and {auxiliary}')
+      make_allowed_move(source, auxiliary)
     elif remainder == 0:
       print(f'Move {i + 1} allowed between {auxiliary} and {target}')
+      make_allowed_move(auxiliary, target)
 
 # initiate call from source A to target C with aux B
 move(NUMBER_OF_DISKS, 'A', 'B', 'C')

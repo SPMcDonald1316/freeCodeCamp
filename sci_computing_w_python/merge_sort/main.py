01f484f1e3d239b9a49bc717feef87f1ab798128
sean <spmcdonald1316@gmail.com>
2024-06-27 13:04:00 -0500

    begin merge process, use while loop to compare elements in both halves and assign smaller element to sorted array

--- sci_computing_w_python/merge_sort/main.py
+++ sci_computing_w_python/merge_sort/main.py
@@ -5,3 +5,17 @@ def merge_sort(array):
 
   merge_sort(left)
   merge_sort(right)
+
+  left_index = 0
+  right_index = 0
+  sort_index = 0
+
+  while left_index < len(left) and right_index < len(right):
+    if left[left_index] < right[right_index]:
+      array[sort_index] = left[left_index]
+      left_index += 1
+    else:
+      array[sort_index] = right[right_index]
+      right_index += 1
+      
+    sort_index += 1
